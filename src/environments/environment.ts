e6aa8e6f9c6cbc1121cc864e883efb6107d9fa6c
Mani <manikanta.tankala@siriuscom.com>
2017-08-13 22:27:59 +0530

    remove env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAhwa7tecR8GPeIYI9YVzdZg0R_-5C9k2Q",
-    authDomain: "anonymous-97084.firebaseapp.com",
-    databaseURL: "https://anonymous-97084.firebaseio.com",
-    projectId: "anonymous-97084",
-    storageBucket: "anonymous-97084.appspot.com",
-    messagingSenderId: "186417701262"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
